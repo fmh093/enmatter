b6b3020bb4c4d64a606f7f8205abff1f662b3eda
Flemming Månsson Hansen <fmh@vertica.dk>
2019-03-15 15:40:41 +0100

    working snapshot

--- Enmatter/Models/Formatters/StacktraceFormatter.cs
+++ Enmatter/Models/Formatters/StacktraceFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,25 @@ namespace Enmatter.Formatters
 
         public override string Translate(string input)
         {
-            return new System.Diagnostics.StackTrace(input);
+            var formattedStacktrace = input;
+
+            var searchReplaceDictionary = new Dictionary<string, string>();
+            searchReplaceDictionary.Add("   at", "\r\n   at");
+            searchReplaceDictionary.Add(" ---> ", "\r\n ---> ");
+            searchReplaceDictionary.Add(" at ", "\r\n at ");
+            searchReplaceDictionary.Add(" --- End of inner exception stack trace ---", "\r\n   --- End of inner exception stack trace ---");
+            searchReplaceDictionary.Add("\r\n\r\n   at", "\r\n   at");
+            searchReplaceDictionary.Add("\r\n\r\n ---> ", "\r\n ---> ");
+            searchReplaceDictionary.Add("\r\n\r\n at ", "\r\n at ");
+            searchReplaceDictionary.Add("\r\n\r\n   --- End of inner exception stack trace ---", "\r\n   --- End of inner exception stack trace ---");
+
+            foreach (var searchReplaceItem in searchReplaceDictionary)
+            {
+                formattedStacktrace = formattedStacktrace.Replace(searchReplaceItem.Key, searchReplaceItem.Value);
+            }
+
+
+            return formattedStacktrace;
         }
     }
 }
